9bb658c36744ad8e500b7c3fc6d171a4aea44ec1
Jefry2310 <CORREO_ASOCIADO_A_Jefry2310>
2025-07-30 18:20:18 -0600

    Reubicacion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-import { auth } from './firebaseConfig'; // Importa el objeto 'auth' de tu configuración de Firebase
+import { auth } from '../firebaseConfig'; // Importa el objeto 'auth' de tu configuración de Firebase
 import { onAuthStateChanged } from 'firebase/auth';
 
 // Importa tus componentes de página
